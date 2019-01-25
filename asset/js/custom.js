3b4aeda9fe5ed3cc6cf144ab10bcbaaa00b8f08b
Kiến Đình Khôi <kdinhkhoi@gmail.com>
2019-01-25 13:05:32 +0700

    Init

---
+++ asset/js/custom.js
@@ -0,0 +1,14 @@
+﻿//Chặn click chột phải
+// document.addEventListener('contextmenu', event => event.preventDefault());
+//Chặn F12 và Ctrl U
+// document.onkeydown = function (e) {
+//     if (e.ctrlKey && (e.keyCode === 85) || (e.keyCode === 123) || e.ctrlKey && (e.keyCode === 16) && (e.keyCode === 73)) {
+//         $("#linkGG").attr("href", "#");
+//         return false;
+//     }
+// };
+//Hiệu ứng waiting cho button
+function pleaseWaitButton(thatButton) {
+    thatButton.disabled = true;
+    thatButton.innerHTML = '<i class="fa fa-spinner fa-spin"></i> Đợi xíu nha...';
+}
